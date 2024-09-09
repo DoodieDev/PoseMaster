920e189a6a0112df0e881d55bc3ab61a7e522bbc
Doodie <alexanderjuhlerkoch@gmail.com>
2024-09-09 16:41:24 +0200

    Init gradle project

---
+++ src/main/java/org/doodieman/poseMaster/PoseMaster.java
@@ -0,0 +1,17 @@
+package org.doodieman.poseMaster;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PoseMaster extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
